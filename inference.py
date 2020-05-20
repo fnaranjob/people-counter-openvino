1a1c94b336c182ff2ebd4fc856ae40cecacc72e1
Fernando Naranjo <fnaranjob@supracnc.com>
2020-05-19 22:08:03 -0500

    generate inputs dictionary

--- inference.py
+++ inference.py
@@ -21,7 +21,7 @@
  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
+import numpy as np
 import os
 import sys
 import logging as log
@@ -32,16 +32,19 @@ class Network:
     """
     Load and configure inference plugins for the specified target devices 
     and performs synchronous and asynchronous modes for the specified infer requests.
+    Warning: To be used only with faster_rcnn_inception_v2 tensorflow model
     """
 
     IE=None
     net=None
     exec_net=None
     device=None
+    input_dict=None #input dictionary ready for inference request
 
     def __init__(self, model_xml):
         self.IE=IECore()
         self.net=IENetwork(model=model_xml,weights=model_xml.replace('xml','bin'))
+        self.net_inputs={}
 
     def __check_layers__(self):
         layers_map = self.IE.query_network(network=self.net,device_name=self.device)
@@ -54,16 +57,19 @@ class Network:
     def load_model(self,device_name):
         self.device=device_name
         if(self.__check_layers__()):
-            print("All layers supported")
             self.exec_net=self.IE.load_network(network=self.net,device_name=device_name,num_requests=1)
         else:
             sys.exit("Unsupported model layer found, can't continue")
 
         #Using OpenVino V2020.1, no need for CPU extensions anymore
 
-    def get_input_shape(self):
-        ### TODO: Return the shape of the input layer ###
-        return
+    def get_inputs(self, processed_image, height, width, scale):
+        info_vec=np.array([[height,width,scale]]) #image info vector
+        input_keys=[]
+        for key in self.net.inputs:
+            input_keys.append(key)
+        input_dict={input_keys[0]:info_vec, input_keys[1]:processed_image}
+        return input_dict
 
     def exec_inference(self):
         ### TODO: Start an asynchronous request ###
