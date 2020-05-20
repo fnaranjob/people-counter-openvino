393ab1fe982eab3554af36d57b3ad0a676bf08c5
Fernando Naranjo <fnaranjob@supracnc.com>
2020-05-19 23:13:56 -0500

    add function to process input image

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
-def process_input():
-	return
+import numpy as np
+import cv2
+
+def process_input(image, height, width):
+	processed_image = np.copy(image)
+	processed_image = cv2.resize(processed_image,(width,height))
+	processed_image = processed_image.transpose((2,0,1))
+	processed_image = processed_image.reshape(1, 3, height, width)
+	return processed_image
 
 def process_output():
 	return
