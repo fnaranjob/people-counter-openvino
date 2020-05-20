340dd8cd399002bb54027cab29b0cf39e66c0c7a
Fernando Naranjo <fnaranjob@supracnc.com>
2020-05-19 23:53:39 -0500

    extract relevant boxes from inference output

--- utils.py
+++ utils.py
@@ -8,5 +8,14 @@ def process_input(image, height, width):
 	processed_image = processed_image.reshape(1, 3, height, width)
 	return processed_image
 
-def process_output():
-	return
+def process_output(inference_output, threshold, input_width, input_height):
+	person_detections = inference_output[inference_output[:,1]==1]
+	person_detections = inference_output[inference_output[:,2]>=threshold]
+	boxes=[]
+	for detection in person_detections:
+		pt1=(int(detection[3]*input_width), int(detection[4]*input_height))
+		pt2=(int(detection[5]*input_width), int(detection[6]*input_height))
+		box = {'pt1':pt1 , 'pt2':pt2}
+		boxes.append(box)
+
+	return boxes
